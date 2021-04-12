300b2f64dc11ced38eff64ee39a4bf707d9f8c2d
Sergio Alvarez <salvare736@gmail.com>
2021-04-12 15:59:32 -0400

    Completed up thru the 'Connect a premade action' task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from './reducers/index';
+import { addOne } from './actions/index';
 
 import './App.css';
 
@@ -7,7 +8,7 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
-  
+
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
@@ -20,10 +21,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b>{state.operation}</span>
+              <span id="memory"><b>Memory:</b>{state.memory}</span>
             </div>
             
             <div className="row">
@@ -33,7 +34,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={() => dispatch(addOne())}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
