722a9bbc64b7c4c57825bf842b7bb35974a4b36c
Sergio Alvarez <salvare736@gmail.com>
2021-04-12 16:11:25 -0400

    Completed up thru the 'Create and connect and action creator' task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from './reducers/index';
-import { addOne } from './actions/index';
+import { addOne, applyNumber, changeOperation } from './actions/index';
 
 import './App.css';
 
@@ -34,27 +34,28 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={() => dispatch(addOne())}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              {/* <CalcButton value={1} onClick={() => dispatch(addOne())}/> */}
+              <CalcButton value={1} onClick={() => dispatch(applyNumber(1))}/>
+              <CalcButton value={2} onClick={() => dispatch(applyNumber(2))}/>
+              <CalcButton value={3} onClick={() => dispatch(applyNumber(3))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => dispatch(applyNumber(4))}/>
+              <CalcButton value={5} onClick={() => dispatch(applyNumber(5))}/>
+              <CalcButton value={6} onClick={() => dispatch(applyNumber(6))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => dispatch(applyNumber(7))}/>
+              <CalcButton value={8} onClick={() => dispatch(applyNumber(8))}/>
+              <CalcButton value={9} onClick={() => dispatch(applyNumber(9))}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={() => dispatch(changeOperation('+'))}/>
+              <CalcButton value={"*"} onClick={() => dispatch(changeOperation('*'))}/>
+              <CalcButton value={"-"} onClick={() => dispatch(changeOperation('-'))}/>
             </div>
 
             <div className="row ce_button">
